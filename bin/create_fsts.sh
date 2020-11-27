e079ad02f3040fd5b076ec881a9d850ebbd572f9
juholeinonen <juho.leinonen@aalto.fi>
2020-11-27 16:27:45 +0200

    removed magic folder names

--- bin/create_fsts.sh
+++ bin/create_fsts.sh
@@ -4,6 +4,7 @@
 bin_folder=asr_with_Docker/bin
 csv_file=$bin_folder/phone-finnish-finnish.csv
 
+version_name=200k_ex3
 smaller_lm=2gram.lm.gz
 larger_lm=3gram.lm.gz
 txt_for_dict=200000.words
@@ -16,17 +17,17 @@ cp ../wsj/s5/path.sh .
 
 sed -i '1 s/...$//' path.sh
 
-cat <<EOF > data/dict/optional_silence.txt
+cat <<EOF > data/"$version_name"_dict/optional_silence.txt
 SIL
 EOF
 
-cat <<EOF > data/dict/silence_phones.txt
+cat <<EOF > data/"$version_name"_dict/silence_phones.txt
 NSN
 SIL
 SPN
 EOF
 
-cat <<EOF > data/dict/nonsilence_phones.txt
+cat <<EOF > data/"$version_name"_dict/nonsilence_phones.txt
 2
 A
 I
@@ -54,11 +55,11 @@ y
 EOF
 
 
-python3 $bin_folder/change_lex_pho.py data/local/lm/$txt_for_dict $csv_file
-mv lexicon.txt data/dict
+python3 $bin_folder/change_lex_pho.py data/local/lm/$version_name/$txt_for_dict $csv_file
+mv lexicon.txt data/"$version_name"_dict
 
 extra=3
-utils/prepare_lang.sh --num-extra-phone-disambig-syms $extra data/dict "<UNK>" data/lang/local data/lang
+utils/prepare_lang.sh --num-extra-phone-disambig-syms $extra data/"$version_name"_dict "<UNK>" data/"$version_name"_lang/local data/"$version_name"_lang
 
 
 # IF USING MORPHS. CHECK https://github.com/aalto-speech/subword-kaldi README
@@ -68,10 +69,10 @@ utils/prepare_lang.sh --num-extra-phone-disambig-syms $extra data/dict "<UNK>" d
 # Overwrite L_disambig.fst
 #common/make_lfst_wb.py $(tail -n$extra $dir/phones/disambig.txt) < $tmpdir/lexiconp_disambig.txt | fstcompile --isymbols=$dir/phones.txt --osymbols=$dir/words.txt --keep_isymbols=false --keep_osymbols=false | fstaddselfloops  $dir/phones/wdisambig_phones.int $dir/phones/wdisambig_words.int | fstarcsort --sort_type=olabel > $dir/L_disambig.fst 
 
-utils/format_lm.sh data/lang data/local/lm/$smaller_lm data/dict/lexicon.txt data/lang/test
-mv data/lang/test/G.fst data/lang/G.fst
+utils/format_lm.sh data/"$version_name"_lang data/local/lm/$version_name/$smaller_lm data/"$version_name"_dict/lexicon.txt data/"$version_name"_lang_test
+mv data/"$version_name"_lang_test/G.fst data/"$version_name"_lang/G.fst
 
-utils/mkgraph.sh $mkgraph_params data/lang exp/nnet3/chain exp/ex3_word/graph
+utils/mkgraph.sh $mkgraph_params data/"$version_name"_lang exp/nnet3/chain exp/"$version_name"/graph
 
 # utils/format_lm.sh data/lang data/local/lm/$larger_lm data/dict/lexicon.txt data/lang_3g
-utils/build_const_arpa_lm.sh data/local/lm/$larger_lm data/lang data/lang_test_3g
+utils/build_const_arpa_lm.sh data/local/lm/"$version_name"/$larger_lm data/"$version_name"_lang data/"$version_name"_const_arpa
